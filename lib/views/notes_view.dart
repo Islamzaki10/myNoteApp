5c316c47fa9e5f245eb441286a9b0e7d6cbe9525
Islamzaki10 <islamzaki27696@gmail.com>
2025-07-08 05:12:28 +0300

    notes cubit

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -13,6 +13,7 @@ class NotesView extends StatelessWidget {
         shape: const CircleBorder(),
         onPressed: () {
           showModalBottomSheet(
+            isScrollControlled: true,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
