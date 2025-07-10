267b9050041505fad3279c6b87ef2b646e97dfe1
Islamzaki10 <islamzaki27696@gmail.com>
2025-07-10 05:08:37 +0300

    delete note

--- lib/views/widgets/note_card.dart
+++ lib/views/widgets/note_card.dart
@@ -48,7 +48,9 @@ class Notecard extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                },
                 icon: Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
